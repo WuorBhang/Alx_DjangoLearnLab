670db617d5993d6d5ae7230e05bad4eea3cdb68a
WuorBhang <uhuribhang211@gmail.com>
2024-08-13 20:46:03 +0300

    updating it now

---
+++ django_models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Book
+
+# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+
+# Alternatively, you can use the following method to register
+# admin.site.register(Book, BookAdmin)
---
+++ django_models/LibraryProject/bookshelf/retrieve.md
@@ -0,0 +1,9 @@
+#!/bin/bash
+# insert headline
+### Retrieve the Book
+
+```python
+book = Book.objects.get(title="1984")
+print(book.title, book.author, book.publication_year)
+# Output: 1984 George Orwell 1949
+
