29384f600b1441c559332ed3845c2035af71b821
WuorBhang <uhuribhang211@gmail.com>
2024-08-13 22:05:50 +0300

    updating it now

--- django-models/django_models/urls.py
+++ django-models/django_models/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('relationship_app/', include('relationship_app.urls')),
 ]
