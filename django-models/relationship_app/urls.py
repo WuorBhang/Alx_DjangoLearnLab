3de908da8cf4ff98558f339d8906aba7208b666c
WuorBhang <uhuribhang211@gmail.com>
2024-08-13 22:11:24 +0300

    updating it now

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from relationship_app import views
+from .views import list_books
 
 urlpatterns = [
     path('admin/', admin.site.urls),
