ffe4687dd71b5f1842ddfe6f67a9863479268cdd
WuorBhang <uhuribhang211@gmail.com>
2024-08-16 11:33:55 +0300

    adding model.py and query_samples.py files

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,71 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-import datetime
-
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    published_date = models.DateField(default=datetime.date(2023, 1, 1))
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
-
-    def __str__(self):
-        return self.title
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 class Library(models.Model):
-    name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200, default="Unknown")
-    books = models.ManyToManyField(Book)  # Ensure that `Book` is referenced correctly
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
 
-    def __str__(self):
-        return self.name
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-
-# creating users authications
-
-class UserProfile(models.Model):
-    USER_ROLES = (
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    )
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=USER_ROLES)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
-    
-
-    # Use Django signals to automatically create a UserProfile whenever a new User is created
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
-
--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,14 +1,16 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author_name = "Author Name"
-author = Author.objects.get(name=author_name)
-books_by_author = Book.objects.filter(author=author)
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
 # List all books in a library
-library_name = "Library Name"
-library = Library.objects.get(name=library_name)  # This line retrieves the library by its name
-books_in_library = library.books.all()
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
 # Retrieve the librarian for a library
-librarian = Librarian.objects.get(library=library)
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
