69106b536bc0a26afb7ff04495a38864051a7622
WuorBhang <uhuribhang211@gmail.com>
2024-08-16 14:11:14 +0300

    adding new files

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, LibraryDetailView, user_register
 
-
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', user_register, name='register'),
+    path('register/', user_register, name='register'), 
 ]
