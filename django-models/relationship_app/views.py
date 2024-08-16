677fad426d0b41273c1b0bebda47f8951d673947
WuorBhang <uhuribhang211@gmail.com>
2024-08-16 14:13:06 +0300

    adding new files

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
+from django.views.generic.detail import DetailView
+
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('list_books')
+        else:
+            return render(request, 'relationship_app/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'relationship_app/login.html')
+
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
 
 def user_register(request):
     if request.method == 'POST':
@@ -12,12 +42,3 @@ def user_register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
