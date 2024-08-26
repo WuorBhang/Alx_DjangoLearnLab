7e57b39725258951e524cd67818d2927d7939a8e
WuorBhang <uhuribhang211@gmail.com>
2024-08-26 14:32:45 +0300

    adding files of CRUD

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import BookList
+
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
