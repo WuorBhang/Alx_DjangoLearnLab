606403527dc7d908d7e9c65767653d1d203339ce
WuorBhang <uhuribhang211@gmail.com>
2024-08-26 16:29:00 +0300

    adding files for api_projects

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the API!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'), 
     path('api/', include('api.urls')),
 ]
