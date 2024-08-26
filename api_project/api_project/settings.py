c67f790c5f111dc8490fe7c35c721a64f302b88c
WuorBhang <uhuribhang211@gmail.com>
2024-08-26 16:45:04 +0300

    adding files for api_projects

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -132,5 +132,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  # Restrict access to authenticated users
+        'IsAuthenticatedOrReadOnly',  # Allow read access to everyone, write access to authenticated users
     ],
 }
