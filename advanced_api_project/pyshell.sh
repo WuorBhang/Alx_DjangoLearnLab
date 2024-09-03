ee70b3c945710aa53bee3c2d486bfbfd84764a06
WuorBhang <uhuribhang211@gmail.com>
2024-09-03 11:45:52 +0300

    updates

--- advanced_api_project/pyshell.sh
+++
@@ -1,9 +0,0 @@
-from api.models import Author, Book
-from api.serializers import AuthorSerializer, BookSerializer
-
-author = Author.objects.create(name="J.K. Rowling")
-book = Book.objects.create(title="Harry Potter", publication_year=1997, author=author)
-
-author_serializer = AuthorSerializer(author)
-print(author_serializer.data)
-
