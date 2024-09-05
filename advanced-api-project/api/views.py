e25572cae49455b6656204d27e910d25bdc26c21
Wuor Bhang <uhuribhang211@gmail.com>
2024-09-05 23:19:08 +0300

    adding advanced api files

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,12 +1,9 @@
-from django_filters import rest_framework
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
-from rest_framework.filters import OrderingFilter
-
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 
@@ -15,11 +12,11 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]  # OrderingFilter included
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author']
-    ordering_fields = ['title', 'publication_year']  # Define fields for ordering
-    
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # Default ordering
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
