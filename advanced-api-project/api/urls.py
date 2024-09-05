27a1dd9103ddb8fbca8a21f6ab072767f4f051a9
Wuor Bhang <uhuribhang211@gmail.com>
2024-09-05 22:26:23 +0300

    adding advanced api files

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
+
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),  # List
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve, 
-    path('books/create/', BookCreateView.as_view(), name='book-create'), # create books,
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'), # update,
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'), # delete a specific book, 
+    # List all books
+    path('books/', BookListView.as_view(), name='book-list'),
+    
+    # Retrieve a single book by ID
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    
+    # Create a new book
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    
+    # Update an existing book by ID
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    
+    # Delete a book by ID
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
