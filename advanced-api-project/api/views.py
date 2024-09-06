21c29747e55e594452c8bd29e86cc9927ca358da
Wuor Bhang <uhuribhang211@gmail.com>
2024-09-06 09:24:27 +0300

    adding advance api files project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
